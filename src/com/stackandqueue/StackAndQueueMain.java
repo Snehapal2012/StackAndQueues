22a2e7d87781f684814e51a756f5042e920bcde3
snehapal2012 <snehapal2012@gmail.com>
2022-07-01 18:40:05 +0530

    Added: Welcome to Stack and Queue problem

---
+++ src/com/stackandqueue/StackAndQueueMain.java
@@ -0,0 +1,7 @@
+package com.stackandqueue;
+
+public class StackAndQueueMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack and Queue Problem");
+    }
+}
